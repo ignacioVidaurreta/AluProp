ec168cff0a859b346e45cdb48d8a60ca51908e1e
Bensas <juanbensa@gmail.com>
2020-04-18 21:16:11 -0300

    Organized project folders

--- aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/app.module.ts
+++ aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
