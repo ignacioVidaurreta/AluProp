00b0fc235d4cff8055c105b23cfcf847683bb774
Bensas <juanbensa@gmail.com>
2020-06-06 10:41:15 -0300

    Changed dummy properties for API endpoint

--- aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/services/property.service.ts
+++ aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/services/property.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { PageRequest } from '../interfaces/page-request';
 import { Observable } from 'rxjs';
 import { Property } from '../models/property';
 import { PageResponse } from '../interfaces/page-response';
 
-const BASE_API_URL = '../assets/json/dummyProperties.json';
+const BASE_API_URL = 'http://localhost:8080/api';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 
+    'Access-Control-Allow-Origin':'*',
+    'Authorization':'authkey',
+    'userid':'1'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +23,6 @@ export class PropertyService {
   constructor(private http: HttpClient) { }
 
   getAll(pageRequest?: PageRequest): Observable<PageResponse<Property>>{
-    return this.http.get<PageResponse<Property>>(BASE_API_URL);
+    return this.http.get<PageResponse<Property>>(BASE_API_URL + '/property/', httpOptions);
   }
 }
