2faef31e3013fcbdb48ed2a3114135d2b07059e8
nachito <igvidaurreta@gmail.com>
2020-06-28 18:29:53 -0300

    endpoint: create endpoint to get owned properties of host

--- aluprop-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HostApiController.java
+++ aluprop-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HostApiController.java
@@ -2,8 +2,11 @@ package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.interfaces.service.ProposalService;
 import ar.edu.itba.paw.interfaces.service.UserService;
+import ar.edu.itba.paw.model.Property;
 import ar.edu.itba.paw.model.Proposal;
 import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.webapp.dto.IndexPropertyDto;
+import ar.edu.itba.paw.webapp.dto.PropertyDto;
 import ar.edu.itba.paw.webapp.dto.ProposalDto;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,6 +16,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Collection;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Path("host")
@@ -31,8 +35,20 @@ public class HostApiController {
         Collection<Proposal> proposals = proposalService.getProposalsForOwnedProperties(user);
 
         return Response.ok(proposals.stream()
-                .map(ProposalDto::fromProposal)
-                .collect(Collectors.toList()))
+                        .map(ProposalDto::fromProposal)
+                        .collect(Collectors.toList()))
+                .build();
+    }
+
+    @GET
+    @Path("/properties")
+    public Response getProperties(){
+        User user = userService.getCurrentlyLoggedUser();
+        Collection<Property> ownedProperties = userService.getWithRelatedEntities(user.getId()).getOwnedProperties();
+
+        return Response.ok(ownedProperties.stream()
+                            .map(IndexPropertyDto::fromProperty)
+                            .collect(Collectors.toList()))
                 .build();
     }
 }
