771797b21afd00622e0216618b02db4dfaaa9550
nachito <igvidaurreta@gmail.com>
2020-06-28 18:38:51 -0300

    endpoint: get interests of guest

--- aluprop-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/GuestApiController.java
+++ aluprop-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/GuestApiController.java
@@ -1,8 +1,10 @@
 package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.interfaces.service.UserService;
+import ar.edu.itba.paw.model.Property;
 import ar.edu.itba.paw.model.User;
 import ar.edu.itba.paw.model.UserProposal;
+import ar.edu.itba.paw.webapp.dto.IndexPropertyDto;
 import ar.edu.itba.paw.webapp.dto.UserProposalDto;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -31,5 +33,17 @@ public class GuestApiController {
                 .map(UserProposalDto::fromUserProposal)
                 .collect(Collectors.toList()))
                 .build();
+    }
+
+    @GET
+    @Path("/interests")
+    public Response getPropertiesInterests(){
+        User user = userService.getCurrentlyLoggedUser();
+        Collection<Property> properties = userService.getWithRelatedEntities(user.getId()).getInterestedProperties();
+
+        return Response.ok(properties.stream()
+                            .map(IndexPropertyDto::fromProperty)
+                            .collect(Collectors.toList()))
+                        .build();
     }
 }
