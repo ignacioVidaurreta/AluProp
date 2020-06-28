10ce0e2fae1a0843f2fdc4e536d651232e444133
nachito <igvidaurreta@gmail.com>
2020-06-28 19:56:49 -0300

    wip endpoint: get creator of proposal userproposal

--- aluprop-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ProposalApiController.java
+++ aluprop-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ProposalApiController.java
@@ -2,11 +2,13 @@ package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.interfaces.service.ProposalService;
 import ar.edu.itba.paw.model.Proposal;
+import ar.edu.itba.paw.model.User;
 import ar.edu.itba.paw.model.UserProposal;
 import ar.edu.itba.paw.webapp.dto.ProposalDto;
 import ar.edu.itba.paw.webapp.dto.UserProposalDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.method.P;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -15,11 +17,14 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Path("proposal")
 @Produces(value = { MediaType.APPLICATION_JSON })
 public class ProposalApiController {
+    private final static Logger logger = LoggerFactory.getLogger(ProposalApiController.class);
 
     @Autowired
     private ProposalService proposalService;
@@ -35,7 +40,7 @@ public class ProposalApiController {
     }
 
     @GET
-    @Path("{proposalId}/userProposals")
+    @Path("/{proposalId}/userProposals")
     public Response userProposalsFromProposal(@PathParam("proposalId") long proposalId){
         Proposal proposal = proposalService.getWithRelatedEntities(proposalId);
 
@@ -44,5 +49,41 @@ public class ProposalApiController {
                             .collect(Collectors.toList()))
                         .build();
 
+    }
+
+    @GET
+    @Path("/{proposalId}/creatorUserProposal")
+    public Response userProposalOfCreator(@PathParam("proposalId") long proposalId){
+        Proposal proposal = proposalService.getWithRelatedEntities(proposalId);
+        if(proposal == null){
+            logger.error("Invalid Proposal with id " + proposalId);
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        User creator = proposal.getCreator();
+        Collection<UserProposal> userProposals = proposal.getUserProposals();
+        if(creator == null || userProposals.isEmpty()){
+            String errorMessage;
+            if(creator == null){
+                errorMessage = "Couldn't find creator of proposal ";
+            }else{
+                errorMessage = "Couldn't find any userProposals for proposal ";
+            }
+            logger.error(errorMessage + proposalId);
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        List<UserProposal> creatorUserProposalList = userProposals.stream().map(up -> {
+           if(up.getUser().equals(creator)){
+               return up;
+           }
+           return null;
+        }).filter(Objects::nonNull).collect(Collectors.toList());
+
+        System.out.println(creatorUserProposalList.size());
+        if(creatorUserProposalList.size() != 1){
+            logger.error("Arrived to impossible state: creatorUserProposalListSize: " + creatorUserProposalList.size());
+            return Response.status(Response.Status.EXPECTATION_FAILED).build();
+        }
+        return Response.ok(UserProposalDto.fromUserProposal(creatorUserProposalList.get(0))).build();
     }
 }
