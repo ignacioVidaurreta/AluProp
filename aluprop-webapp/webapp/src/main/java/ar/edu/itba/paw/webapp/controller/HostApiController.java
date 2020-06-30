4d6852627962ebb393067ff2b33d7794448dd751
nachito <igvidaurreta@gmail.com>
2020-06-29 22:16:51 -0300

    wip endpoint: create property

--- aluprop-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HostApiController.java
+++ aluprop-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HostApiController.java
@@ -1,22 +1,22 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.interfaces.Either;
+import ar.edu.itba.paw.interfaces.service.PropertyService;
 import ar.edu.itba.paw.interfaces.service.ProposalService;
 import ar.edu.itba.paw.interfaces.service.UserService;
 import ar.edu.itba.paw.model.Property;
 import ar.edu.itba.paw.model.Proposal;
 import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.webapp.dto.ErrorDto;
 import ar.edu.itba.paw.webapp.dto.IndexPropertyDto;
 import ar.edu.itba.paw.webapp.dto.PropertyDto;
 import ar.edu.itba.paw.webapp.dto.ProposalDto;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Collection;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Path("host")
@@ -27,6 +27,8 @@ public class HostApiController {
     UserService userService;
     @Autowired
     ProposalService proposalService;
+    @Autowired
+    PropertyService propertyService;
 
     @GET
     @Path("/proposals")
@@ -50,5 +52,18 @@ public class HostApiController {
                             .map(IndexPropertyDto::fromProperty)
                             .collect(Collectors.toList()))
                 .build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/createProperty")
+    public Response createProperty(Property property){
+        Either<Property,Collection<String> > maybeProperty = propertyService.create(property);
+
+        if(!maybeProperty.hasValue()){
+            Response.status(Response.Status.CONFLICT).entity(ErrorDto.fromErrors(maybeProperty.alternative())).build();
+        }
+
+        return Response.status(Response.Status.CREATED).entity(PropertyDto.fromProperty(maybeProperty.value())).build();
     }
 }
