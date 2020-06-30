eb19ba93e9fbc3076360bc7281e2e68526d35454
nachito <igvidaurreta@gmail.com>
2020-06-30 14:29:48 -0300

    Add ErrorDTO

---
+++ aluprop-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/dto/ErrorDto.java
@@ -0,0 +1,25 @@
+package ar.edu.itba.paw.webapp.dto;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class ErrorDto {
+    private ErrorDto() { }
+
+    private ArrayList<String> errors;
+
+    public static ErrorDto fromErrors(Collection<String> errors){
+        ErrorDto ret = new ErrorDto();
+        ret.errors = new ArrayList<>(errors);
+
+        return ret;
+    }
+
+    public ArrayList<String> getErrors() {
+        return errors;
+    }
+}
