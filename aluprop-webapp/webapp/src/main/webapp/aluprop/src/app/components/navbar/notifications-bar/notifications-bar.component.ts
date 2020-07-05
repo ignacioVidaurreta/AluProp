cdb740a9b99eea22b8424c2626f0f19d0afeb707
Bensas <juanbensa@gmail.com>
2020-07-05 17:38:29 -0300

    Notifications menu now looks better and closes on outside click

--- aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/components/navbar/notifications-bar/notifications-bar.component.ts
+++ aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/components/navbar/notifications-bar/notifications-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-notifications-bar',
@@ -9,6 +9,13 @@ export class NotificationsBarComponent implements OnInit {
 
   showNotifications = false;
 
+  @HostListener('document:click', ['$event']) 
+  onDocumentClick(event) {
+    if (this.showNotifications === true && event.path.filter(
+        (elem)=> elem.id === 'toggle-notifications').length !== 1){
+      this.showNotifications = false;
+    }
+  }
 
   constructor() { }
 
