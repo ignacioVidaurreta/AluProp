0a231bfe573bc702883baadf87ab8abe3872dc72
clara <cguzzetti@itba.edu.ar>
2020-07-05 16:15:27 -0300

    Error handling in create proposal and createProposal connected to backend

--- aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/services/proposal.service.ts
+++ aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/services/proposal.service.ts
@@ -5,8 +5,9 @@ import {Observable} from "rxjs";
 import {Proposal} from "../models/proposal";
 import {UserProposal} from "../models/userProposal";
 import {User} from "../models/user";
+import {Property} from "../models/property";
 
-const BASE_API_URL = '../assets/json/dummyProposal.json';
+const BASE_API_URL = 'http://localhost:8080/api/';
 const BASE_API_URL_PROPOSAL = 'http://localhost:8080/api/proposal/';
 const BASE_API_URL_PROPOSAL_USER_INFO = 'http://localhost:8080/api/guest/';
 
@@ -35,5 +36,11 @@ export class ProposalService {
 
   getGuestUserInfoByProposalId(id: Number): Observable<any>{
     return this.http.get<any>(BASE_API_URL_PROPOSAL_USER_INFO+id+'/userInfo');
+  }
+
+  createProposal(invitedUserIds: number[], propertyId: number): Observable<Proposal> {
+    console.log('Creating proposal:');
+    console.log(invitedUserIds);
+    return this.http.post<Proposal>(BASE_API_URL + 'guest/proposal/' + propertyId, invitedUserIds);
   }
 }
