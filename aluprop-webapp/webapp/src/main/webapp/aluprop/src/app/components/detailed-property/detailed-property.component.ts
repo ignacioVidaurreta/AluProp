4f3eee1bf7238c060577b8097c061ef5c5ffd27e
clara <cguzzetti@itba.edu.ar>
2020-07-05 19:52:31 -0300

    Change availability connected to endpoint in detailed property

--- aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/components/detailed-property/detailed-property.component.ts
+++ aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/components/detailed-property/detailed-property.component.ts
@@ -33,6 +33,7 @@ export class DetailedPropertyComponent implements OnInit {
   isUserLoggedIn: boolean;
   isUserLoggedInSub: Subscription;
   languageChangedSub: Subscription;
+  changePropertyStatusSub: Subscription;
 
   constructor(private propertyService: PropertyService, private userService: UserService, private translateService: TranslateService, private metadataService: MetadataService, private authenticationService: AuthenticationService, public dialog: MatDialog, private route: ActivatedRoute) {
     this.languageChangedSub = translateService.onLangChange.subscribe((newLang) => this.translateRulesAndServices());
@@ -47,7 +48,7 @@ export class DetailedPropertyComponent implements OnInit {
     this.dropSubscriptions();
   }
 
-  onPageChange(pageEvent: PageEvent){
+  onPageChange(){
     this.dropSubscriptions();
     this.createPageSubscription();
   }
@@ -58,30 +59,31 @@ export class DetailedPropertyComponent implements OnInit {
     if (this.interestedUsersSub){ this.interestedUsersSub.unsubscribe() };
     if (this.isUserLoggedInSub){ this.isUserLoggedInSub.unsubscribe() };
     if (this.currentUserIsInterestedSub){ this.currentUserIsInterestedSub.unsubscribe() };
+    if (this.changePropertyStatusSub){ this.changePropertyStatusSub.unsubscribe() }
   }
 
   createPageSubscription(){
     this.propertySub = this.propertyService.getById(this.propertyId).subscribe((property) => {
       this.property = property;
       this.translateRulesAndServices();
-      console.log(property);
+      console.log(property.availability);
       this.isUserLoggedInSub = this.userService.isUserLoggedIn().subscribe((isUserLoggedIn) => {
         this.isUserLoggedIn = isUserLoggedIn;
         if(this.isUserLoggedIn) {
           this.currentUserSub = this.authenticationService.getCurrentUser().subscribe((currentUser)=> {
             this.currentUser = currentUser;
-            console.log(currentUser);
+            // console.log(currentUser);
             this.interestedUsersSub = this.propertyService.getInterestedUsersByPropertyId(this.propertyId).subscribe((interestedUsers) => {
             this.interestedUsers = interestedUsers;
-            console.log(interestedUsers);
-            var index = interestedUsers.map(function(user) { return user.id; }).indexOf(this.currentUser.id);
-            console.log(interestedUsers.splice(index,1));
-            console.log(interestedUsers);
+            // console.log(interestedUsers);
+            var index = interestedUsers.map(function(user) { return user?.id; }).indexOf(this.currentUser.id);
+            // console.log(interestedUsers.splice(index,1));
+            // console.log(interestedUsers);
             this.interestedUsersWithoutCurrentUser = interestedUsers;
             });
             if(this.currentUser?.role == 'ROLE_GUEST') {
               this.currentUserIsInterestedSub = this.propertyService.isCurrentUserInterested(this.propertyId).subscribe((currentUserIsInterested) => {
-                console.log(currentUserIsInterested);
+                // console.log(currentUserIsInterested);
                 this.currentUserIsInterested = currentUserIsInterested;
               });
               }
@@ -116,5 +118,12 @@ export class DetailedPropertyComponent implements OnInit {
   translateRulesAndServices(){
     this.metadataService.translateMetadataArray(this.property.rules);
     this.metadataService.translateMetadataArray(this.property.services);
+  }
+
+  changePropertyAvailability() {
+    console.log(this.property.availability);
+    this.changePropertyStatusSub = this.propertyService.changePropertyAvailability(this.property.id).subscribe(
+      () => {this.onPageChange();}
+    );
   }
 }
--- aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/services/property.service.ts
+++ aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/services/property.service.ts
@@ -48,12 +48,19 @@ export class PropertyService {
     return this.http.post<Property>(BASE_API_URL + 'host/createProperty/', property);
   }
 
-  getInterestedUsersByPropertyId(id: number): Observable<User[]>{
-    console.log(BASE_API_URL_PROPERTY + id);
+  getInterestedUsersByPropertyId(id: number): Observable<User[]> {
+    // console.log(BASE_API_URL_PROPERTY + id);
     return this.http.get<User[]>(BASE_API_URL_PROPERTY + id + '/interestedUsers');
   }
 
-  isCurrentUserInterested(id: number): Observable<boolean>{
+  isCurrentUserInterested(id: number): Observable<boolean> {
     return this.http.get<boolean>(BASE_API_URL_PROPERTY_IS_CURRENT_USER_INTERESTED+id);
   }
+
+  changePropertyAvailability(propertyId: number) {
+    console.log('Pausing property:');
+    console.log(propertyId);
+    return this.http.post(BASE_API_URL + 'host/changeStatus/' + propertyId, {});
+  }
+
 }
