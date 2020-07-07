af2955ed17c71cb43e50c171ddfc3c96926caee2
Bensas <juanbensa@gmail.com>
2020-07-07 13:23:40 -0300

    Improved handling of error responses with HttpInterceptor

--- aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/app-routing.module.ts
+++ aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/app-routing.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
     path: 'error/:code',
     component: ErrorComponent,
   },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: 'error/404' }
 ];
 
 @NgModule({
--- aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/services/token-interceptor.ts
+++ aluprop-webapp/webapp/src/main/webapp/aluprop/src/app/services/token-interceptor.ts
@@ -4,18 +4,24 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpResponse,
+  HttpErrorResponse
 } from '@angular/common/http';
 import { AuthenticationService } from './authentication.service';
-import { Observable } from 'rxjs';
+import { Observable, EMPTY } from 'rxjs';
+import { tap, filter, catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthenticationService) { }
+  constructor(private authService: AuthenticationService,
+              private router: Router) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (this.authService.getAuthToken()){
+      // console.log(request);
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${this.authService.getAuthToken()}`
@@ -23,6 +29,20 @@ export class TokenInterceptor implements HttpInterceptor {
       });
     }
     
-    return next.handle(request);
+    return next.handle(request).pipe(
+      filter(response => response instanceof HttpResponse),
+      tap(
+        (response) => {
+           console.log(response);
+        }),
+      catchError(
+        (response: HttpErrorResponse) => {
+          if (response.status === 404){
+            console.log(response);
+            this.router.navigate(['/error/404']);
+          }
+          return EMPTY;
+        })
+    );
   }
 }
