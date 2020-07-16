ebe988754ed28fef8985c7dbdc6ebbc574ddf1f5
clara <cguzzetti@itba.edu.ar>
2020-07-16 03:10:59 -0300

    Notifications title translated

--- aluprop-webapp/webapp/src/main/aluprop/src/app/components/notifications/notifications.module.ts
+++ aluprop-webapp/webapp/src/main/aluprop/src/app/components/notifications/notifications.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/modules/material.module';
 import { NotificationsComponent } from './notifications.component';
 import { RouterModule, Routes } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 const routes: Routes = [{path: '', component: NotificationsComponent}];
 const routing: ModuleWithProviders = RouterModule.forChild(routes);
@@ -12,6 +13,7 @@ const routing: ModuleWithProviders = RouterModule.forChild(routes);
   imports: [
     CommonModule,
     MaterialModule,
+    TranslateModule,
     routing
   ]
 })
